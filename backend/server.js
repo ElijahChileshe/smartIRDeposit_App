b7dea0d60c451f9a726ae491fe0afc3375a6015a
Elijah Chileshe <chilesheelijah01@gmail.com>
2023-10-22 12:03:26 +0200

    Added mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,38 @@
-const express = require("express")
-require('dotenv').config()
-
+const express = require("express");
+require('dotenv').config();
+const user = require("./routes/user/user");
+const registration =  require("./routes/registration/registration")
+const mongoose = require("mongoose")
 
 // express app
 const app = express()
 
-
+// middleware
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next()
 })
+
+app.use(express.json())
+
+
+
+// routes
+app.use(user);
+app.use(registration);
+
+
+// Database setup
+mongoose.connect(process.env.MONGODB_CONNECTION, {useNewUrlParser: true,
+useNewUrlParser: true,
+// useCreateIndex: true,
+useUnifiedTopology: true,
+    }).then(()=> {
+    console.log("Successfully Connected to Cloud Database")
+}).catch((err) =>{
+    console.log(err);
+});
+
 
 
 // port
