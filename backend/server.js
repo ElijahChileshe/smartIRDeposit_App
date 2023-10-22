c7cd922c37ef233d1e85da9d316ea114cfe0a5d5
Elijah Chileshe <chilesheelijah01@gmail.com>
2023-10-22 12:17:45 +0200

    Revert "Added mongoose connection"
    This reverts commit b7dea0d60c451f9a726ae491fe0afc3375a6015a.

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,15 @@
-const express = require("express");
-require('dotenv').config();
-const user = require("./routes/user/user");
-const registration =  require("./routes/registration/registration")
-const mongoose = require("mongoose")
+const express = require("express")
+require('dotenv').config()
+
 
 // express app
 const app = express()
 
-// middleware
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next()
 })
-
-app.use(express.json())
-
-
-
-// routes
-app.use(user);
-app.use(registration);
-
-
-// Database setup
-mongoose.connect(process.env.MONGODB_CONNECTION, {useNewUrlParser: true,
-useNewUrlParser: true,
-// useCreateIndex: true,
-useUnifiedTopology: true,
-    }).then(()=> {
-    console.log("Successfully Connected to Cloud Database")
-}).catch((err) =>{
-    console.log(err);
-});
-
 
 
 // port
